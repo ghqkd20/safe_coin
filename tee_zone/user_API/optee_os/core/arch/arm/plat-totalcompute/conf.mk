09d1e5d8c684041ad1486557796ef8b2a4de01be
taehjoon <zxc8283@daum.net>
2021-08-14 20:26:58 +0900

    optee_os

---
+++ tee_zone/user_API/optee_os/core/arch/arm/plat-totalcompute/conf.mk
@@ -0,0 +1,45 @@
+PLATFORM_FLAVOR ?= tc0
+
+ifeq ($(PLATFORM_FLAVOR),tc0)
+include core/arch/arm/cpu/cortex-armv8-0.mk
+platform-debugger-arm := 1
+endif
+
+$(call force,CFG_WITH_ARM_TRUSTED_FW,y)
+$(call force,CFG_GENERIC_BOOT,y)
+ifeq ($(CFG_CORE_SEL2_SPMC),y)
+$(call force,CFG_GIC,n)
+$(call force,CFG_ARM_GICV3,n)
+else
+$(call force,CFG_GIC,y)
+$(call force,CFG_ARM_GICV3,y)
+endif
+$(call force,CFG_PL011,y)
+$(call force,CFG_PM_STUBS,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_ARM64_core,y)
+$(call force,CFG_WITH_LPAE,y)
+
+ifeq ($(platform-debugger-arm),1)
+# ARM debugger needs this
+platform-cflags-debug-info = -gdwarf-2
+platform-aflags-debug-info = -gdwarf-2
+endif
+
+ifeq ($(PLATFORM_FLAVOR),tc0)
+CFG_TEE_CORE_NB_CORE = 8
+
+ifeq ($(CFG_CORE_SEL1_SPMC),y)
+CFG_TZDRAM_START ?= 0xfd000000
+CFG_TZDRAM_SIZE  ?= 0x02000000
+else ifeq ($(CFG_CORE_SEL2_SPMC),y)
+CFG_TZDRAM_START ?= 0xfd281000
+CFG_TZDRAM_SIZE  ?= 0x01d7f000
+else
+CFG_TZDRAM_START ?= 0xff000000
+CFG_TZDRAM_SIZE  ?= 0x01000000
+endif
+
+CFG_SHMEM_START  ?= 0xfce00000
+CFG_SHMEM_SIZE   ?= 0x00200000
+endif
